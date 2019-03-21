6253127fe60c7cb56ca44211d70492ba798dc2d0
MichaelRavid <Michael.Ravid@hotmail.com>
2019-03-21 12:47:24 -0400

    youtube migration pt1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,57 @@
-// alert("This is a test");
+// Hip
+
+function playEpisodeh2() {
+  var episodeh1 = `<iframe width="100%" height="315" src="https://www.youtube.com/embed/RouYErl0G9w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+  document.getElementById("hh-content").innerHTML = episodeh1;
+}
+function playEpisodeh13() {
+  var episodeh13 = `<iframe width="100%" height="315" src="https://www.youtube.com/embed/OTRJr96fO6U" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+  document.getElementById("hh-content").innerHTML = episodeh13;
+}
+function playEpisodeh14() {
+  var episodeh14 = `<iframe width="100%" height="315" src="https://www.youtube.com/embed/5cbRZ5sXHyA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+  document.getElementById("hh-content").innerHTML = episodeh14;
+}
+
+// Chicano
+function playEpisodech10() {
+  var episodech10 = `<iframe width="100%" height="315" src="https://www.youtube.com/embed/aUDiJwNrrcM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+  document.getElementById("ch-content").innerHTML = episodech10;
+}
+
+// Country
+function playEpisodeco10() {
+  var episodeco10 = `<iframe width="100%" height="315" src="https://www.youtube.com/embed/yHhe_AL0VaU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+  document.getElementById("co-content").innerHTML = episodeco10;
+}
+
+// Skit
+function playSkitRRR() {
+  var skitRRR = `<iframe width="100%" height="315" src="https://www.youtube.com/embed/nMWP5aaMnL0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+  document.getElementById("s-content").innerHTML = skitRRR;
+}
+
+function playSkitBBA() {
+  var skitBBA = `<iframe width="100%" height="315" src="https://www.youtube.com/embed/rk_zpZyFpes" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+  document.getElementById("s-content").innerHTML = skitBBA;
+}
+
+function playSkitFDB() {
+  var skitFDB = `<iframe width="100%" height="315" src="https://www.youtube.com/embed/b2CN14AtFic" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+  document.getElementById("s-content").innerHTML = skitFDB;
+}
+
+function playSkitVPC() {
+  var skitVPC = `<iframe width="100%" height="315" src="https://www.youtube.com/embed/TfaW4SX4li4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+  document.getElementById("s-content").innerHTML = skitVPC;
+}
+
+function playSkitVPE() {
+  var skitVPE = `<iframe width="100%" height="315" src="https://www.youtube.com/embed/w2MhyFaFwf0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+  document.getElementById("s-content").innerHTML = skitVPE;
+}
+
+function playSkitDRP() {
+  var skitDRP = `<iframe width="100%" height="315" src="https://www.youtube.com/embed/DVYFPbqRQaA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+  document.getElementById("s-content").innerHTML = skitDRP;
+}
